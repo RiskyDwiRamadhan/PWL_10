45b1f265394aabe53a251e805e6a35405949449f
RiskyDwiRamadhan <riskydwiramadhan956@gmail.com>
2021-04-13 18:30:17 +0700

    Membuat table articles

--- database/migrations/2021_04_13_044001_create_articles_table.php
+++ database/migrations/2021_04_13_044001_create_articles_table.php
@@ -16,7 +16,7 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
             $table-> string('title');
-            $table-> string('conten');
+            $table-> string('content');
             $table-> string('feature_image');
             $table->timestamps();
         });
